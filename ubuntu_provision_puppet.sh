e61e6e8c30b922536cda809f87c023af115390ab
Vince Broz <vince@broz.cc>
2014-09-05 16:51:57 -0400

    Add missing Ruby dependency for puppet advanced features

--- ubuntu_provision_puppet.sh
+++ ubuntu_provision_puppet.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo Running ubuntu_provision_puppet.sh as $0
+sudo apt-get install -y libaugeas0 libaugeas-dev
 if ! type puppet >/dev/null 2>&1
 then
-  gem install puppet
+  gem install puppet ruby-augeas
 fi
