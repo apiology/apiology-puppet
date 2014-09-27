a7f3bb5b475ac588aa69552ead4535849ba719da
Vince Broz <vince@broz.cc>
2014-09-26 23:25:53 -0400

    Try to fix rvmsudo situation

--- update_ruby.sh
+++ update_ruby.sh
@@ -1,22 +1,25 @@
-#!/bin/bash -e
+#!/bin/bash
 
 RUBY_VERSION=2.1.3
 
-echo Running ubuntu_provision_rvm.sh as $0
-
-rvm requirements run
-rvm get staple
-rvm reload
-rvm cleanup all
-
+echo Running update_ruby.sh as $0
 
 if [[ -s "$HOME/.rvm/scripts/rvm" ]]
 then
   . "$HOME/.rvm/scripts/rvm"
+  RVM="rvm"
 elif [ -f /etc/profile.d/rvm.sh ]
 then
   . /etc/profile.d/rvm.sh
+  RVM="rvmsudo rvm"
+else
+  exit 1
 fi
+
+$RVM reload
+$RVM requirements run
+$RVM get staple
+$RVM cleanup all
 
 interrogate_ubuntu() {
   OS_VERSION=`lsb_release -r -s`
@@ -50,7 +53,7 @@ interrogate_arch() {
   fi
 }
 
-HAS_RUBY=`rvmsudo rvm list | grep ruby-$RUBY_VERSION | wc -l`
+HAS_RUBY=`$RVM list | grep ruby-$RUBY_VERSION | wc -l`
 if [ $HAS_RUBY == 0 ]
 then
   # If this doesn't work, see 'Ruby' simplenote on how to upload binary.
@@ -58,7 +61,7 @@ then
   interrogate_os
   RVM_FILE=ruby-${RUBY_VERSION}.tar.bz2
   BINARY=binaries/${OS_NAME}/${OS_VERSION}/${ARCH}/${RVM_FILE}
-  if ! rvmsudo rvm mount -r http://rvm-binaries-apiology.s3.amazonaws.com/$BINARY
+  if ! $RVM mount -r http://rvm-binaries-apiology.s3.amazonaws.com/$BINARY
   then
     echo "Need to install a new binary ruby into http://rvm-binaries-apiology.s3.amazonaws.com/"
     echo "Please put the following into an issue report at https://github.com/apiology/apiology-puppet/issues:"
@@ -70,10 +73,10 @@ then
   fi
 fi
 
-rvm alias delete default
-rvm alias create default ruby-${RUBY_VERSION}
-rvm use default
-rvm uninstall ruby-2.1.2
-#  rvmsudo rvm install --default $PREFERRED_VERSION
+$RVM alias delete default
+$RVM alias create default ruby-${RUBY_VERSION}
+$RVM use default
+$RVM uninstall ruby-2.1.2
+#  $RVM install --default $PREFERRED_VERSION
 ruby -v
 type bundle 2>/dev/null || gem install bundler
