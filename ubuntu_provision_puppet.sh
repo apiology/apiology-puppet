98787a52c5f21cc2ac8e893e573abf3811a79d03
Vince Broz <vince@broz.cc>
2014-10-01 21:37:04 -0400

    Add rvmsudo

--- ubuntu_provision_puppet.sh
+++ ubuntu_provision_puppet.sh
@@ -4,5 +4,5 @@ echo Running ubuntu_provision_puppet.sh as $0
 sudo apt-get install -y libaugeas0 libaugeas-dev
 if ! type puppet >/dev/null 2>&1
 then
-  gem install puppet ruby-augeas
+  rvmsudo gem install puppet ruby-augeas
 fi
