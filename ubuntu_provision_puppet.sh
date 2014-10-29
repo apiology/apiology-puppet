d8cd16c41167434094bd0d34186d5c72fff66a80
Vince Broz <vince@broz.cc>
2014-10-28 21:21:59 -0400

    Use rvmsudo to determine if puppet is there

--- ubuntu_provision_puppet.sh
+++ ubuntu_provision_puppet.sh
@@ -2,7 +2,7 @@
 
 echo Running ubuntu_provision_puppet.sh as $0
 sudo apt-get install -y libaugeas0 libaugeas-dev
-if ! type puppet >/dev/null 2>&1
+if ! rvmsudo sh -c "type puppet" >/dev/null 2>&1
 then
   rvmsudo gem install puppet ruby-augeas
 fi
