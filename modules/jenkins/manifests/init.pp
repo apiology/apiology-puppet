4d8df2d3cb8871a5417934c04d031c4d8e59326f
Vince Broz <vince@broz.cc>
2014-12-30 13:18:23 -0500

    More configurability

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -5,27 +5,28 @@ class jenkins::conf {
   $move_jar = "mv WEB-INF/jenkins-cli.jar ${jenkins_cli_jar}"
   $remove_dir = 'rm -rf WEB-INF'
 
-  exec { 'jenkins-cli' :
-    command => "${extract_jar} && ${move_jar} && ${remove_dir}",
+  exec { 'Extract jenkins-cli jar' :
+    command => "service jenkins start && ${extract_jar} && ${move_jar} && ${remove_dir}",
     path    => ['/bin', '/usr/bin'],
     cwd     => '/tmp',
     creates => $jenkins_cli_jar,
-    require => Service['jenkins'],
+    require => Package['jenkins'],
   }
 
   file { $jenkins_cli_jar:
     ensure  => file,
-    require => Exec['jenkins-cli'],
+    require => Exec['Extract jenkins-cli jar'],
   }
 
 }
 
-define jenkins_job($module, $relative_path = '', $use_basic_auth = true, $username = '', $password = '') {
+define jenkins_job($module, $relative_path = '', $use_auth = false, $username = '', $password = '') {
   include jenkins::conf
-  $credentials_string = $use_basic_auth ? {
-    true => '',
-    false => "--username $username --password '$password'"
+  $credentials_string = $use_auth ? {
+    false => '',
+    true => "--username $username --password '$password'"
   }
+
   file { "/tmp/${name}.job":
     mode => 644,
     owner => "jenkins",
@@ -33,6 +34,7 @@ define jenkins_job($module, $relative_path = '', $use_basic_auth = true, $userna
     notify => Exec["${name}"],
     source => "puppet:///modules/${module}/${name}.job"
   }
+
   exec { "${name}":
     command => "/usr/bin/java -jar $jenkins::conf::jenkins_cli_jar -s http://localhost:8080/${relative_path} delete-job \"${name}\" ${credentials_string}; /usr/bin/java -jar $jenkins::conf::jenkins_cli_jar -s http://localhost:8080/${relative_path} create-job \"${name}\" ${credentials_string} < \"/tmp/${name}.job\"",
     user => 'jenkins',
@@ -44,40 +46,58 @@ define jenkins_job($module, $relative_path = '', $use_basic_auth = true, $userna
   }
 }
 
-define jenkins_plugin($version, $relative_path = '') {
+define jenkins_plugin($version, $relative_path = '', $use_auth = false, $username = '', $password = '') {
   include jenkins::conf
+  $credentials_string = $use_auth ? {
+    true => "--username $username --password '$password'",
+    false => ''
+  }
   $written_git_jpi = $operatingsystemrelease ? {
     older => "/var/lib/jenkins/plugins/download/plugins/${name}/${version}/${name}.hpi",
     default => "/var/lib/jenkins/plugins/${name}.jpi"
   }
-  exec {
-    "/usr/bin/java -jar $jenkins::conf::jenkins_cli_jar -s http://localhost:8080/${relative_path} install-plugin http://updates.jenkins-ci.org/download/plugins/${name}/${version}/${name}.hpi -restart":
-    user => 'jenkins',
-    require => [Service["jenkins"],File[$jenkins::conf::jenkins_cli_jar]],
-    tries => 6,
-    try_sleep => 90,
-    unless => "/bin/ls $written_git_jpi"
+  exec { "wait for jenkins - $name":
+    command => "/usr/bin/wget --spider --tries 10 --retry-connrefused http://localhost:8080/${relative_path}cli",
+    returns => [0, 8],
+    require => [Package["jenkins"],File[$jenkins::conf::jenkins_cli_jar]]
+  }
+  exec { "install jenkins plugin - $name":
+      command => "/usr/bin/java -jar $jenkins::conf::jenkins_cli_jar -s http://localhost:8080/${relative_path} install-plugin ${credentials_string} http://updates.jenkins-ci.org/download/plugins/${name}/${version}/${name}.hpi -restart",
+      user => 'jenkins',
+      require => [Exec["wait for jenkins - $name"],File[$jenkins::conf::jenkins_cli_jar]],
+      tries => 8,
+      try_sleep => 15,
+      unless => "/bin/ls $written_git_jpi";
   }
 }
 
-define jenkins($site = $name, $certfile = $name, $relative_path = '', $use_basic_auth = true) { # 'ssl-cert-snakeoil' is another popular choice
+define jenkins($site = $name, $certfile = $name, $relative_path = '', $use_auth = false, $username = '', $password = '', $stable = true) { # 'ssl-cert-snakeoil' is another popular choice
   include apache2
   include wget
+  if $stable == true {
+    $apt_flag_file = "/var/lib/apt/lists/pkg.jenkins-ci.org_debian-stable_binary_Packages"
+    $apt_key_url = "http://pkg.jenkins-ci.org/debian-stable/jenkins-ci.org.key"
+    $jenkins_list_source = "puppet:///modules/jenkins/jenkins-stable.list"
+  } else {
+    $apt_flag_file = "/var/lib/apt/lists/pkg.jenkins-ci.org_debian_binary_Packages"
+    $apt_key_url = "http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key"
+    $jenkins_list_source = "puppet:///modules/jenkins/jenkins.list"
+  }
   exec { 'install_jenkins_key':
-    command => "/usr/bin/wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo /usr/bin/apt-key add -",
+    command => "/usr/bin/wget -q -O - $apt_key_url | sudo /usr/bin/apt-key add -",
     require => Package['wget'],
     unless => "/usr/bin/apt-key list | grep kk@kohsuke.org"
   }
   exec { 'Extra jenkins apt-get update':
     command => '/usr/bin/apt-get update',
     require => File['/etc/apt/sources.list.d/jenkins.list'],
-    creates => "/var/lib/apt/lists/pkg.jenkins-ci.org_debian-stable_binary_Packages"
+    creates => "$apt_flag_file";
   }
   file { "/etc/apt/sources.list.d/jenkins.list":
     owner => root,
     group => root,
     mode => 0644,
-    source => "puppet:///modules/jenkins/jenkins.list",
+    source => $jenkins_list_source,
     require => Exec[install_jenkins_key],
   }
   package { "jenkins":
@@ -95,8 +115,8 @@ define jenkins($site = $name, $certfile = $name, $relative_path = '', $use_basic
   }
   file { "/etc/default/jenkins":
     mode => 644,
-    owner => "root",
-    group => "root",
+    owner => root,
+    group => root,
     require => Package["jenkins"],
     content => template('jenkins/etc-default-jenkins.erb'),
     notify => Service["jenkins"],
@@ -110,10 +130,16 @@ define jenkins($site = $name, $certfile = $name, $relative_path = '', $use_basic
     require => Package[apache2];
   }
   jenkins_plugin { 'git':
+    use_auth => $use_auth,
+    username => $username,
+    password => $password,
     version => '1.2.0',
     relative_path => $relative_path
   }
   jenkins_plugin { 'git-client':
+    use_auth => $use_auth,
+    username => $username,
+    password => $password,
     version => '1.0.2',
     relative_path => $relative_path
   }
