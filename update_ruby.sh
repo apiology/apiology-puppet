fd654730827d00b57905b7057d335458186452f0
Vince Broz <vince@broz.cc>
2015-05-30 17:07:40 -0400

    Fix bug on OS X

--- update_ruby.sh
+++ update_ruby.sh
@@ -65,7 +65,7 @@ interrogate_arch() {
 }
 
 HAS_RUBY=`$RVM list | grep ruby-$DESIRED_RUBY_VERSION | wc -l`
-if [ "$HAS_RUBY" == 0 ]
+if [ $HAS_RUBY == 0 ] # if $HAS_RUBY is in quotes, it won't be found on OS X, where wc -l gives whitespace before and after output
 then
   # If this doesn't work, see 'Ruby' simplenote on how to upload binary.
   interrogate_arch
