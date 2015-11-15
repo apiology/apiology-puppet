ae217bbf88d5f65c77ebac6975fa1e73175a98d1
Vince Broz <vince@broz.cc>
2015-11-15 15:53:27 -0500

    Add missing dependency for puppet 4

--- ubuntu_provision_puppet.sh
+++ ubuntu_provision_puppet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Running ubuntu_provision_puppet.sh as $0
-sudo apt-get install -y libaugeas0 libaugeas-dev
+sudo apt-get install -y libaugeas0 libaugeas-dev libgmp-dev
 if ! rvmsudo sh -c "type puppet" >/dev/null 2>&1
 then
   rvmsudo gem install puppet ruby-augeas
